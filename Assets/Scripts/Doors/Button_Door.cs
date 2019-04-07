5326079c06363e661bba281e0bc09f7f1a0000dc
Unknown <pokepeel15@yahoo.co.uk>
2019-04-07 16:54:17 +0100

    Added Multipoint Platforms, colliders and the initial door system
    Altered and cleaned up scripts, added the first itteration of the door system. Added the multipoint platforms.

--- Assets/Scripts/Doors/Button_Door.cs
+++ Assets/Scripts/Doors/Button_Door.cs
@@ -4,91 +4,107 @@ using UnityEngine;
 
 public class Button_Door: MonoBehaviour {
 
-	//doors
-	public GameObject door_left;
+    //Button Materials
+    public Material m_pressed;
+
+    //doors
+    public GameObject door_left;
 	public GameObject door_right;
-	//doors move to here
+	
+    //doors move to here
 	public GameObject door_left_target;
 	public GameObject door_right_target;
 
-	public GameObject button;
-	public GameObject button_trigger;
-	public GameObject main_camera;
+   
+    public GameObject button;
+	public GameObject button_target;
+    public GameObject player_target;
 
-	private GameObject player_character;
+    //cameras
+    public GameObject main_camera;
+
+	private GameObject player;
 
 	private bool interaction = false;
 	private bool move_doors = false;
 	private bool move_button = false;
 	private bool triggered = false;
 
-	private float button_pos_z = 0;
-	private float door_speed = 10f;
+	private float door_speed = 7.5f;
 
 	void Start ()
 	{
-		player_character = GameObject.FindGameObjectWithTag("Player");
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	void FixedUpdate()
 	{
-		interaction = Input.GetButton("Interaction");
+		interaction = Input.GetButton("Attack1");
 	}
 
 	private void OnTriggerStay(Collider other)
 	{
-		if (other.gameObject == player_character)
+		if (other.gameObject == player)
 		{
 			Debug.Log ("Player Enter Door Trigger");
 		}
 
-		if (other.gameObject == player_character && interaction && !triggered)
+		if (other.gameObject == player && interaction && !triggered)
 		{
-			//player_character.GetComponent<Character_Movement>().SetInteraction (true);
-			//player_character.GetComponent<Character_Movement>().SetDisabled (true);
-			player_character.transform.position = new Vector3 (button_trigger.transform.position.x, 0.05f, button_trigger.transform.position.z);
-			player_character.transform.rotation = Quaternion.Euler (0f, 0f, 0f);
-			//main_camera.GetComponent<MainCamController>().SetDoorCam(1);
-			button_pos_z = button.transform.position.z + 0.05f;
-			StartCoroutine("ButtonDelay");
+            float desiredAngle = transform.eulerAngles.y;
+            //player_character.GetComponent<Character_Movement>().SetDisabled (true);
+            player.transform.position = new Vector3(player_target.transform.position.x, player.transform.position.y, player_target.transform.position.z);
+            player.transform.rotation = Quaternion.Euler(0, desiredAngle, 0);
+            //main_camera.GetComponent<MainCamController>().SetDoorCam(1);
+            StartCoroutine("ButtonDelay");
 			triggered = true;
-		}
+        }
 	}
 
 	void Update()
 	{
 		float delta_speed = door_speed * Time.deltaTime;
 
-		if (move_doors)
-		{
-			door_left.transform.position = Vector3.MoveTowards (transform.position, door_left_target.transform.position, delta_speed);
-			door_right.transform.position = Vector3.MoveTowards (transform.position, door_right_target.transform.position, delta_speed);
-
-			if (door_left.transform.position == door_left_target.transform.position && door_right.transform.position == door_right_target.transform.position)
-			{
-				StartCoroutine("PlayerRestartDelay");
-				move_doors = false;
-			}
-		}
-
-		if (move_button)
-		{
-			button.transform.position = new Vector3 (button.transform.position.x, button.transform.position.y ,button.transform.position.z + 0.1f * Time.deltaTime);
-
-			if (button.transform.position.z >= button_pos_z)
-			{
-			//	main_camera.GetComponent<MainCamController>().SetDoorCam(2);
-				StartCoroutine("DoorDelay");
-				move_button = false;
-			}
-		}
+        MoveButton();
+        MoveDoors(delta_speed);
 	}
+
+    private void MoveButton()
+    {
+        if (move_button)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, button_target.transform.position, 0.3f * Time.deltaTime);
+
+            if (button.transform.position == button_target.transform.position)
+            {
+                //	main_camera.GetComponent<MainCamController>().SetDoorCam(2);
+                this.GetComponent<MeshRenderer>().material = m_pressed;
+                StartCoroutine("DoorDelay");
+                move_button = false;
+            }
+        }
+    }
+
+    private void MoveDoors(float delta_speed)
+    {
+        if (move_doors)
+        {
+            door_left.transform.position = Vector3.MoveTowards(door_left.transform.position, door_left_target.transform.position, delta_speed);
+            door_right.transform.position = Vector3.MoveTowards(door_right.transform.position, door_right_target.transform.position, delta_speed);
+
+            if (door_left.transform.position == door_left_target.transform.position && door_right.transform.position == door_right_target.transform.position)
+            {
+                StartCoroutine("PlayerRestartDelay");
+                move_doors = false;
+            }
+        }
+    }
 
 	IEnumerator ButtonDelay()
 	{
-		yield return new WaitForSeconds (0.05f);
-		move_button = true;
-	}
+		yield return new WaitForSeconds (0.075f);
+        move_button = true;
+    }
 
 	IEnumerator DoorDelay()
 	{
